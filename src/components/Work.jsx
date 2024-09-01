3813c150b5cb05598a5d9ee61424654ac07ffe0a
sujeettx <sujeetkushwaha0123@gmail.com>
2024-09-01 23:05:46 +0530

    flip animatation added in work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { useNavigate } from "react-router";
-const portifolio = require('./assets/portfolioimage.webp')
-const chatApp =  require('./assets/chat-image.jpg');
-const sportClub = require('./assets/sport-image.jpg');
-const weatherApp = require('./assets/frontend-image.avif');
+const portifolio = require("./assets/portfolioimage.webp");
+const chatApp = require("./assets/chat-image.jpg");
+const sportClub = require("./assets/sport-image.jpg");
+const weatherApp = require("./assets/frontend-image.avif");
 
 function Work() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="work-container Contact-container">
       <p>Workplace</p>
@@ -17,23 +17,61 @@ function Work() {
         collection of projects showcasing my <br />
         experties in full stack development
       </p>
-      <div className="work-boxes">
-        <div className="work-boxes-child">
-          <img src={portifolio} alt=""/>
+      <div className="work-card">
+        <div className="flip-card">
+          <div className="front-card">
+            <img src={portifolio} alt="" />
+          </div>
+          <div className="back-card">
+            <h2>back side</h2>
+          </div>
         </div>
-        <div className="work-boxes-child">
-        <img src={chatApp} alt=""/>
+
+
+        <div className="flip-card">
+          <div className="front-card">
+            <img src={chatApp} alt="" />
+          </div>
+          <div className="back-card">
+            <h2>back side</h2>
+          </div>
         </div>
-        <div className="work-boxes-child">
-        <img src={sportClub} alt=""/>
+
+
+        <div className="flip-card">
+          <div className="front-card">
+            <img src={weatherApp} alt="" />
+          </div>
+          <div className="back-card">
+            <h2>back side</h2>
+          </div>
         </div>
-        <div className="work-boxes-child">
-        <img src={weatherApp} alt=""/>
+
+        <div className="flip-card">
+          <div className="front-card">
+            <img src={sportClub} alt="" />
+          </div>
+          <div className="back-card">
+            <h2>back side</h2>
+          </div>
         </div>
+
+
       </div>
-      <div className="btn work-btn" onClick={()=>navigate('/')} style={{backgroundColor:'black',color:"white"}}>
-       <h3> <button>Show more </button></h3>
-        <ArrowForwardIcon/>
+
+
+
+
+      <div
+        className="btn work-btn"
+        onClick={() => navigate("/")}
+        style={{ backgroundColor: "black", color: "white" }}
+      >
+        <h3>
+          {" "}
+          <button>Show more </button>
+        </h3>
+        <ArrowForwardIcon />
       </div>
     </div>
   );
